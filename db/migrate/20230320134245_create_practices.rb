7be13cead9b39dbc911b9a2863dd969e5b763652
Ved Tiwari <ved@webkorps.com>
2023-03-20 19:16:14 +0530

    created prectice model with name and age

---
+++ db/migrate/20230320134245_create_practices.rb
@@ -0,0 +1,10 @@
+class CreatePractices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :practices do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
